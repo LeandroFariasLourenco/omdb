2ff9219b727b664710b316f046a788d0c6bfda87
Leandro <leandro.farias01@outlook.com>
2020-02-23 23:20:52 -0300

    refactor/Adding development files

---
+++ src/imdb-index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import "./imdb-index.scss";
+import { App } from "./components/App";
+import { Header } from "./components/Header";
+
+ReactDOM.render(<Header/>,document.querySelector("header"));
