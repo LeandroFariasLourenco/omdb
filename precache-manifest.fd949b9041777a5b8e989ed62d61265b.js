9b15dac2f46a4117eada49d24ff075cb4a60cdd4
Leandro <leandro.farias01@outlook.com>
2020-03-02 10:04:55 -0300

    Updates

---
+++ precache-manifest.fd949b9041777a5b8e989ed62d61265b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ccbf349d8fb8b20c47d6f9821552a0",
+    "url": "/omdb/index.html"
+  },
+  {
+    "revision": "a409fcfe4b100fd81394",
+    "url": "/omdb/static/css/main.49501bf0.chunk.css"
+  },
+  {
+    "revision": "ffda99fa6de9dab4c2e3",
+    "url": "/omdb/static/js/2.3d1d69f0.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "/omdb/static/js/2.3d1d69f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a409fcfe4b100fd81394",
+    "url": "/omdb/static/js/main.e9157b6b.chunk.js"
+  },
+  {
+    "revision": "a2dc654e973f2eff0e4a",
+    "url": "/omdb/static/js/runtime-main.95dd5224.js"
+  },
+  {
+    "revision": "01f40a3af8bfadc3287cddf37b35463a",
+    "url": "/omdb/static/media/movie_logo.01f40a3a.png"
+  },
+  {
+    "revision": "3f7d6a655b54339a63f25006e260bad3",
+    "url": "/omdb/static/media/react_logo.3f7d6a65.png"
+  },
+  {
+    "revision": "091bf88ab19547360376dc24014addfd",
+    "url": "/omdb/static/media/spinner.091bf88a.gif"
+  }
+]);
